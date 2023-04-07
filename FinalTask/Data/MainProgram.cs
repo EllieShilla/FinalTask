f43f6bd27b8da095a9d8b1ffa3a40e34f3693ad2
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2023-04-07 15:51:30 +0300

    Cleaned Main method

---
+++ FinalTask/Data/MainProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask.Data
+{
+    public class MainProgram
+    {
+        FileReader fileReader;
+        FileRecorder fileRecorder;
+        public MainProgram()
+        {
+            fileReader = new FileReader();
+            fileRecorder = new FileRecorder();
+        }
+
+        public void WorkProgram()
+        {
+            if (fileReader.ReadFile() != null)
+            {
+                List<College> colleges = fileReader.ReadFile();
+
+                if (fileRecorder.WriteSpecializationsToFile(DataProcessor.GetSpezializations(colleges)))
+                    Console.WriteLine(@"Spezializations are written to a 'file1.txt'.");
+
+                if (fileRecorder.WriteToFileOrderBySalaryEmployees(DataProcessor.GetEmployeesOrderBySalary(DataProcessor.GetCollegeByMaxStudentsCount(colleges))))
+                    Console.WriteLine(@"Sorted college employees saved to a 'file2.txt'.");
+            }
+        }
+    }
+}
